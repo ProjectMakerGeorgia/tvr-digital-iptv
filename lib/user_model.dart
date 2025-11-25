ab9689d17d0494ba3dcc570ef9fbd8bb5ab166f6
radiolagames <radiolagames@gmail.com>
2025-11-25 19:47:58 +0000

    feat: Update UserModel to match database schema

--- lib/user_model.dart
+++ lib/user_model.dart
@@ -1,22 +1,50 @@
-class User {
-  final String userId;
-  final String username;
-  final String email;
-  final String token;
 
-  User({
-    required this.userId,
+class UserModel {
+  final int id;
+  final String username;
+  final String? firstName;
+  final String? lastName;
+  final String? email;
+  final String? phone;
+  final String? playlistUrl;
+  final String? token;
+  final double balance;
+  final int userstatus;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final int? chatId;
+
+  UserModel({
+    required this.id,
     required this.username,
-    required this.email,
-    required this.token,
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.phone,
+    this.playlistUrl,
+    this.token,
+    required this.balance,
+    required this.userstatus,
+    required this.createdAt,
+    required this.updatedAt,
+    this.chatId,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      userId: json['user_id'] as String,
-      username: json['username'] as String,
-      email: json['email'] as String,
-      token: json['token'] as String,
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      username: json['username'],
+      firstName: json['first_name'],
+      lastName: json['last_name'],
+      email: json['email'],
+      phone: json['phone'],
+      playlistUrl: json['playlist_url'],
+      token: json['token'],
+      balance: double.parse(json['balance'].toString()),
+      userstatus: json['userstatus'],
+      createdAt: DateTime.parse(json['created_at']),
+      updatedAt: DateTime.parse(json['updated_at']),
+      chatId: json['chat_id'],
     );
   }
 }
